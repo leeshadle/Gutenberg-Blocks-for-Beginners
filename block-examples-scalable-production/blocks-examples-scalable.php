f13240e1ff14810a14ea25cc50ad6a5d622f587e
Lee Shadle <leeshadle@gmail.com>
2018-07-12 10:20:33 -0700

    The block-examples-scalable-production plugin.
    The source code for the block-examples-scalable-production plugin we build together in the Gutenberg Blocks for Beginners course.

---
+++ block-examples-scalable-production/blocks-examples-scalable.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+* Plugin Name: Block Examples Scalable Production
+* Description: This is a plugin demonstrating how to build blocks using a more scalable, easier to maintain architecture.
+* Text Domain: block-examples-scalable
+* Version: 0.1.0
+**/
+
+//Prevent direct access
+defined( 'ABSPATH' )  || exit;
+
+add_action('plugins_loaded', 'block_examples_scalable_loader');
+
+function block_examples_scalable_loader() {
+	/* Load the blocks */
+	require_once plugin_dir_path(__FILE__) . './dist/init.php';
+}
